2d29bbc83726b40b14dde371ceaee9a49b494240
Muhammad Huzefa Ali <muhammadhali2003@gmail.com>
2025-06-28 17:09:16 -0700

    Placeholder

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -667,7 +667,7 @@ const DegreeProposal = () => {
                     <iframe
                       width="100%"
                       height="100%"
-                      src="https://www.youtube.com/embed/OqPKCkr54hQ"
+                      src="https://www.youtube.com/watch?v=ScMzIvxBSi4"
                       title="ISCI Program Information"
                       frameBorder="0"
                       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -790,7 +790,7 @@ const DegreeProposal = () => {
           </p>
           <div className="flex gap-4">
             <Button variant="outline" size="sm" asChild>
-              <a href="https://github.com/your-repository-link" target="_blank" rel="noopener noreferrer">
+              <a href="https://github.com/Maqmood-Zafi/iscidegreeproposal" target="_blank" rel="noopener noreferrer">
                 <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.387.6.11.82-.258.82-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.757-1.333-1.757-1.089-.744.083-.729.083-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.834 2.809 1.304 3.495.997.108-.775.418-1.305.762-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.468-2.381 1.235-3.221-.123-.303-.535-1.523.117-3.176 0 0 1.008-.322 3.301 1.23a11.52 11.52 0 0 1 3.003-.404c1.018.005 2.042.138 3.003.404 2.292-1.552 3.301-1.23 3.301-1.23.653 1.653.241 2.873.118 3.176.768.84 1.235 1.911 1.235 3.221 0 4.61-2.803 5.625-5.475 5.921.43.372.823 1.102.823 2.222v3.293c0 .322.218.694.825.576C20.565 22.092 24 17.594 24 12.297 24 5.373 18.63.297 12 .297z" />
                 </svg>
