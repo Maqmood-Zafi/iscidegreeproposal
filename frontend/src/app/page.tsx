f64ad7c6b31a385cb4e568d91b83889b4c8aebe2
Muhammad Huzefa Ali <muhammadhali2003@gmail.com>
2025-06-30 16:34:48 -0700

    Startup Message Removal

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -851,36 +851,7 @@ const DegreeProposal = () => {
       <div className="max-w-7xl mx-auto p-6">
         {activeTab === 'builder' && (
           <>
-            {/* Startup notification */}
-            {showStartupMessage && (
-              <div className="fixed top-4 left-4 bg-blue-100 border-l-4 border-blue-500 p-5 rounded shadow-lg max-w-md z-50 animate-fade-in">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <svg className="h-6 w-6 text-blue-500" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-                    </svg>
-                  </div>
-                  <div className="ml-3 flex-grow">
-                    <p className="text-base font-medium text-blue-800">
-                      Please allow up to 90 seconds for Backend service to start up on initial load
-                    </p>
-                  </div>
-                  <div className="ml-auto pl-3">
-                    <div className="-mx-1.5 -my-1.5">
-                      <button
-                        onClick={() => setShowStartupMessage(false)}
-                        className="inline-flex bg-blue-50 rounded-md p-1.5 text-blue-500 hover:bg-blue-100 focus:outline-none"
-                      >
-                        <span className="sr-only">Dismiss</span>
-                        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                          <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            )}
+            
             
             <div className="flex flex-col lg:flex-row gap-6">
               {/* Main content area */}
